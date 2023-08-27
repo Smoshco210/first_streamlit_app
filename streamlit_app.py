e467f92ee75d0aa768d41f6a8d83158f98c7bc6e
Smoshco210 <136576810+Smoshco210@users.noreply.github.com>
2023-08-27 19:05:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if not fruit_choice:
 except URLError as e:
 streamlit.error()
 
-streamlit.write('The user entered ', fruit_choice)
+# streamlit.write('The user entered ', fruit_choice)
 
 # import requests
 
